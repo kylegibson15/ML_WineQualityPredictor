e1adc63ac80a374ad6c222505122c181da239495
kylegibson15 <kylegibson15@gmail.com>
2017-12-27 22:29:57 -0700

    remove unecessary prints

--- wine_predictor.py
+++ wine_predictor.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import pickle
+
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
@@ -10,10 +12,6 @@ from sklearn.externals import joblib
 
 dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
 data = pd.read_csv(dataset_url, sep=';')
-
-# print('DATA.HEAD : ', data.head())
-# print('DATA.SHAPE : ', data.shape)
-# print('DATA.DESCRIBE : ', data.describe())
 
 y = data.quality
 X = data.drop('quality', axis=1)
@@ -26,13 +24,22 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
 scaler = preprocessing.StandardScaler().fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 
-# print(X_train_scaled.mean(axis=0))
-# print(X_train_scaled.std(axis=0))
-
 X_test_scaled = scaler.transform(X_test)
-
-# print(X_test_scaled.mean(axis=0))
-# print(X_test_scaled.std(axis=0))
 
 pipeline = make_pipeline(preprocessing.StandardScaler(),
                          RandomForestRegressor(n_estimators=100))
+hyperparameters = { 'randomforestregressor__max_features' : ['sqrt'],
+                  'randomforestregressor__max_depth': [None]}
+clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print('R2_SCORE : ', r2_score(y_test, y_pred))
+print('MEAN_SQ_ERR : ', mean_squared_error(y_test, y_pred))
+
+joblib.dump(clf, 'rf_regressor.pkl')
+clf2 = joblib.load('rf_regressor.pkl')
+
+# Predict data set using loaded model
+clf2.predict(X_test)
